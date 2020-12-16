e544fdccc7351e52eb9364118aa21d9b053d1b3f
tranquang229 <tranquang229@gmail.com>
2020-12-16 18:44:17 +0700

    update

--- Facebook.DAL/Constants/Constant.cs
+++ Facebook.DAL/Constants/Constant.cs
@@ -49,185 +49,80 @@ namespace Facebook.DAL.Constants
 
         public static List<TokenCookie> LIST_TOKEN_COOKIE = new List<TokenCookie>
         {
+            //kwangtran229@gmail.com
+            new TokenCookie
+            {
+                Token = "EAAAAZAw4FxQIBAK0O7RFBYQTZCngT9AdZBGiRZAO5gq75NlmSou5zCiQiIMa0bMABtKeVQVO8h49abMbwMbNIhj5LPKbjdzu8SsGYbXh99MgDMpAJMaXGTU1dnZCQmznRgoXZBPtZAy3iB6kmY5RsaNL4f8WR09QaOTQG9YGZBfvyRv6MqmBbXWWsxZBreKEzol0ZD",
+                Cookie = "sb=uuZAX8zu-ekbY50TQ0DuoH86; datr=uuZAX6_FYi0IBe4DqXYkedR-; dpr=1.25; wd=1536x722; locale=vi_VN; c_user=100016551455570; xs=29%3A9U2fJbvf8GPIjA%3A2%3A1599061415%3A17580%3A636; fr=1cABxZlerDaDKX7To.AWVndLV3wx7Bo2DS7fowySuDqEY.BfQOa6.fr.AAA.0.0.BfT72n.AWWrO_gj; spin=r.1002607989_b.trunk_t.1599061419_s.1_v.2_"
+            },
+            
+            //tranquanghero@yahoo.com-muathudep216@gmail.com
+            new TokenCookie
+            {
+                Token = "EAAAAZAw4FxQIBAMTpYUjogfku73peW119skGxYOmdOZA9TjdHvFqkCa0XhKahDLK1rWZCjnZA7dIzhxaNoYSm1GlpNVHyUnXQjE0unbDtZB2HyZA2QV4ZAFlLJvCfHeUleXosusB1p8IgdRSxljjoP3HKmpJmAMn7IdLUvBiAcbtGKr8kTwlPhm",
+                Cookie = "sb=uuZAX8zu-ekbY50TQ0DuoH86; datr=uuZAX6_FYi0IBe4DqXYkedR-; dpr=1.25; wd=1536x722; locale=vi_VN; c_user=100016551455570; xs=29%3A9U2fJbvf8GPIjA%3A2%3A1599061415%3A17580%3A636; fr=1cABxZlerDaDKX7To.AWVndLV3wx7Bo2DS7fowySuDqEY.BfQOa6.fr.AAA.0.0.BfT72n.AWWrO_gj; spin=r.1002607989_b.trunk_t.1599061419_s.1_v.2_"
+            },
+            
+            //tranquang229@gmail.com
+            new TokenCookie
+            {
+                Token = "EAAAAZAw4FxQIBACowBOcDEf7NXNny3SFIxoK7UVlovOvbZBVUDZBjqmxgXrQnVOc9JcefBcu5C8L8qgCIuzpU3ZBZATvFp17YuiD0LkkA1PZCpgEEqQGausPZCmQLaytipJvCbegZAo0nZC7ZCEDtBPmZCJ4hoLSBCRDApGmuLrOeQjzZCLOyGsOKJs1dTlLj3nCJTQZD",
+                Cookie = "sb=uuZAX8zu-ekbY50TQ0DuoH86; datr=uuZAX6_FYi0IBe4DqXYkedR-; dpr=1.25; wd=1536x722; locale=vi_VN; c_user=100016551455570; xs=29%3A9U2fJbvf8GPIjA%3A2%3A1599061415%3A17580%3A636; fr=1cABxZlerDaDKX7To.AWVndLV3wx7Bo2DS7fowySuDqEY.BfQOa6.fr.AAA.0.0.BfT72n.AWWrO_gj; spin=r.1002607989_b.trunk_t.1599061419_s.1_v.2_"
+            },
+          
             //khovideptrai04@gmail.com
             new TokenCookie
             {
-                Token = "EAAAAZAw4FxQIBACmrkiiUJJUMtDGQm8oSVmM4077PhMj3xiUqoHh0BbGFuj4cePev4R0Raj3KDnWRma4hH0nuCU8pSs4XNzyEj0MnaCNqXa4ZBuFjboAdZAqeQoItuTSPBgvoZBaF5CtWk561NA8Yu1u0VclqDiSJxdFuGc7LAuVLntqTiarJn1ES6kp7RkZD",
+                Token = "EAAAAZAw4FxQIBADzZB9caMrYQyvIza6YfsSE252O3Ulhs3qnUc51vSJpwwM236fKBGuYT9N6UB7S6TVnr09rPDPELquhZB8uhPGsDzHeIvClj71nwpIZARrQrlUM3rLRdleLh5ZBHU4DyOvP2OpToJ0caA6QwLa6WHs73raCwQ1mx2Qoo9cT56CuVcRttuc4ZD",
                 Cookie = "sb=uuZAX8zu-ekbY50TQ0DuoH86; datr=uuZAX6_FYi0IBe4DqXYkedR-; dpr=1.25; wd=1536x722; locale=vi_VN; c_user=100016551455570; xs=29%3A9U2fJbvf8GPIjA%3A2%3A1599061415%3A17580%3A636; fr=1cABxZlerDaDKX7To.AWVndLV3wx7Bo2DS7fowySuDqEY.BfQOa6.fr.AAA.0.0.BfT72n.AWWrO_gj; spin=r.1002607989_b.trunk_t.1599061419_s.1_v.2_"
             },
-            ////khovideptrai09@gmail.com
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBAG9IB7ZCvulgjRNhXlw1khZAZAmQ2glgB1lTIHlE54ZCO5NpdfjjOAGlpkRee2vvEJ3pGsAcuZAo7cHyHqfKShON2uAZAhnxy0QXSHD87xa89t1nEfRp8Eo8IGqnamxlf46DVwwXIpjBGOujGE1Q8CmNJCb12wcqNQ5PBf23FU",
-            //    Cookie = "sb=3OdAX7-_30q14o8FM6wxvH4B; datr=3OdAX32ewY5QokK37f06V8ej; dpr=1.25; wd=1536x722; locale=vi_VN; c_user=100017242360945; xs=46%3AN5oMBzzaLPTX5Q%3A2%3A1599061311%3A17580%3A636; fr=1wikEc4cqO3smfMOl.AWU6K1uFSZxuJv2nx1NvxTVd1bg.BfQOfc.86.AAA.0.0.BfT70_.AWUrxOvk; spin=r.1002607989_b.trunk_t.1599061314_s.1_v.2_"
-            //},
+           
+            //khovideptrai09@gmail.com
+            new TokenCookie
+            {
+                Token = "EAAAAZAw4FxQIBAM4gcTeKggMezTfVnd0mKBm1ctBbOZBhnj4H0ti2kf45QEO8TWRkCkhvhelcq57Cq7eEgw8fEcbeMIfq0xSzlD4PlzBqpOVCaboo0KHCHzGJCdd8ZAZAIyZAfNZCZA2tLZApmUbcgBQ4ZB3zdcZCN6zCqeAvZB1lYjxn8l8nlaB0Hp",
+                Cookie = "sb=uuZAX8zu-ekbY50TQ0DuoH86; datr=uuZAX6_FYi0IBe4DqXYkedR-; dpr=1.25; wd=1536x722; locale=vi_VN; c_user=100016551455570; xs=29%3A9U2fJbvf8GPIjA%3A2%3A1599061415%3A17580%3A636; fr=1cABxZlerDaDKX7To.AWVndLV3wx7Bo2DS7fowySuDqEY.BfQOa6.fr.AAA.0.0.BfT72n.AWWrO_gj; spin=r.1002607989_b.trunk_t.1599061419_s.1_v.2_"
+            },
+            
             //muathudep01@gmail.com
             new TokenCookie
             {
-                Token = "EAAAAZAw4FxQIBAKJbyWhZB1neUZASMSUJVWcZCe2vRKhvpc99ImoBK5dtM7ABwPjUFxL9FlpS5byltR0lYiJqfzNVZBUvORSNR6zMkNdSaAheIwCS4P7t935uouqG0ZBASEgbwdHGnvDxPOUhVXOJ3nDqHAET4G6cvx4WZC7MmBaCepVZB7EoyZCJ",
-                Cookie = "sb=n-hAX396lzIO7D4Qzi21xJO1; datr=n-hAXyksBRvWKWspd-uxbQK0; dpr=1.25; wd=1075x726; locale=vi_VN; c_user=100012631336879; xs=2%3ARI7E9ufl2D6-Yw%3A2%3A1599015392%3A17580%3A6308; fr=1lRS8RluQ4xfmc4Hb.AWUMFcfBs3BLl8O6dGBz2agqMyg.BfQOif.74.AAA.0.0.BfTwng.AWUZillw; spin=r.1002605491_b.trunk_t.1599015397_s.1_v.2_"
+                Token = "EAAAAZAw4FxQIBAD9ZBlPZBJkPEa7oOOZBIer43zkZCTm5ZBim6JJ9ZCtpZBNihctTmd9RXmMKtioQvVMH3h6ed5sMg8FeCIZAwAHKpGEzmM3lsgQqvKkNuDIm786QCzBh4PLCGtV3ZB1cx3LLzqSc45cMCem6P2kCEr28AVr6KEZCV7YmpjbRJrdH90",
+                Cookie = "sb=uuZAX8zu-ekbY50TQ0DuoH86; datr=uuZAX6_FYi0IBe4DqXYkedR-; dpr=1.25; wd=1536x722; locale=vi_VN; c_user=100016551455570; xs=29%3A9U2fJbvf8GPIjA%3A2%3A1599061415%3A17580%3A636; fr=1cABxZlerDaDKX7To.AWVndLV3wx7Bo2DS7fowySuDqEY.BfQOa6.fr.AAA.0.0.BfT72n.AWWrO_gj; spin=r.1002607989_b.trunk_t.1599061419_s.1_v.2_"
             },
+            
             //muathudep06@gmail.com
             new TokenCookie
             {
-                Token = "EAAAAZAw4FxQIBAM50gzlczxZCsdJambqujUJKwgCRYTk1KKrS93NtbAbU1NLZAoZBRqAcGlOCuRJgOEaP034DanTRDZCzBq5W0DdYGLJgZBnoxOpS3sJvppKRYRk3FHZBJGKiFvVtE8LFVZBmUPsFFCeXIk5KZBpwhFW76AX7EuTYRQJCapzBSGqxEaEzvwp5mWAZD",
-                Cookie = "sb=2-pAX-4BtsV1a06Pg56OuMkr; datr=2-pAX1WnmjAqzZxJnNauYIkF; dpr=1.25; locale=vi_VN; wd=1536x722; c_user=100012897726970; xs=20%3AiSb-xGrkUA85fQ%3A2%3A1599015488%3A17580%3A636; fr=1uUGLItvMtEMzn0Tj.AWVLg6bh5QsW66vJ91BbEnnyyl4.BfQOrb.ft.AAA.0.0.BfTwo_.AWUHYNV3; spin=r.1002605491_b.trunk_t.1599015491_s.1_v.2_"
+                Token = "EAAAAZAw4FxQIBAEL35DwSq6tvHvVMwNuan6qnLCZASqokVGyDsAYIlhpuZA9gbgLJCN60PXthTQpvwZA3lFUAr9eihaBOznhYiaAH0h4ZBkkNrzsVpDpj3wbIGrbgPMKjEaJP3b2fBpxxFWfHKt7Bf487UgFYS8vJSpiWrg8ZBfqM5BDsJ3bMfQBd1kTa1fMoZD",
+                Cookie = "sb=uuZAX8zu-ekbY50TQ0DuoH86; datr=uuZAX6_FYi0IBe4DqXYkedR-; dpr=1.25; wd=1536x722; locale=vi_VN; c_user=100016551455570; xs=29%3A9U2fJbvf8GPIjA%3A2%3A1599061415%3A17580%3A636; fr=1cABxZlerDaDKX7To.AWVndLV3wx7Bo2DS7fowySuDqEY.BfQOa6.fr.AAA.0.0.BfT72n.AWWrO_gj; spin=r.1002607989_b.trunk_t.1599061419_s.1_v.2_"
             },
             
-            //muathudep21@gmail.com
+            //muathudep21
             new TokenCookie
             {
-                Token = "EAAAAZAw4FxQIBAHNDZCq2avObgZCnFd87pdymIaF4o9aDDlGkakbY69sSwnnowm7YDuHyEAXC0w18AwoM6D5qlT09VU2o5Rnpu4klQicZBJ3r93BVczlpcEhNXSt7C8KYhwix45rGLuQ2HnGaZBgNxZAhDKcZBMOt1bXis3MJHuIX5uKslIC4bC",
-                Cookie = "sb=d7RAX2fx0c0x_WZb0eG3z33V; datr=d7RAX7PvUOAvD7Sl-Ogs-mc5; c_user=100013246775477; spin=r.1002596222_b.trunk_t.1598925093_s.1_v.2_; dpr=1.25; wd=1536x722; xs=46%3AdQVmnOaP9mOq2Q%3A2%3A1598286312%3A17580%3A636%3A%3AAcWE-7b89hLZy2XMBBvdyxNNT-ruGuEBsrLi0IrIxA; fr=1IA7Sa6ya4buePLzR.AWVi3DY9pjnZhPuH0zzT3FV-cw8.BfQLR3.xM.AAA.0.0.BfTnwf.AWVXYzb1"
+                Token = "EAAAAZAw4FxQIBAEqZASNjZA6To7DuEMZBY4G9vEEtjULlABAmg0TjvJXO3tArWBlcSVZCGRayOwCZBIdpT6WVaIkROHzJa0OtqWoxutXozYnm1BvnN7c46wOEEjABuvJEdPMdiC3uPWx3wnd0IeVYhpVo8ajVJUS58SLmRBhuqxDnkN5ZC9GVrP",
+                Cookie = "sb=uuZAX8zu-ekbY50TQ0DuoH86; datr=uuZAX6_FYi0IBe4DqXYkedR-; dpr=1.25; wd=1536x722; locale=vi_VN; c_user=100016551455570; xs=29%3A9U2fJbvf8GPIjA%3A2%3A1599061415%3A17580%3A636; fr=1cABxZlerDaDKX7To.AWVndLV3wx7Bo2DS7fowySuDqEY.BfQOa6.fr.AAA.0.0.BfT72n.AWWrO_gj; spin=r.1002607989_b.trunk_t.1599061419_s.1_v.2_"
             },
-            ////muathudep22@gmail.com
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBAAJYRVqZBd2fFPDtWuIVgTbG0R4rfiVidWzA0J4bRZCzC1uSCtubFkZCvGAlY8c2ifU5XN33ZAamfvM5sGlZBffZBFj8LZBUcnkQ9zqSLlnZBAj4RRTZB8mY10EhQAGW7BdSTwZCpKhtsGBrT5XejnQgbzmdNqV49KHWr3mslGGSqP",
-            //    Cookie = "sb=cbRAX0dTshVapXy-BQpk51SE; datr=cbRAXxNqdDaRRHYRFBrQ9-CF; c_user=100013338030072; spin=r.1002596222_b.trunk_t.1598925126_s.1_v.2_; dpr=1.25; xs=16%3A-tkurfENBgvzzA%3A2%3A1598286378%3A17580%3A636%3A%3AAcV5ZOGJXCq64Fv3CLiGjy6wV_NRjlScnDrSjrvEKg; fr=1FaN4yqDuHJHuRrck.AWUQCa9EmSDzBn8SRK-MLGwY7a8.BfQLRx.j1.AAA.0.0.BfToRv.AWUTTru4"
-            //},
-            //muathudep75@gmail.com
+             //muathudep22
             new TokenCookie
             {
-                Token = "EAAAAZAw4FxQIBAEZCyOFtzySj3kH6v0J9LNvIckIZCuP8rt1VBAZBRLlq5kC3DT8rXlarBvYx0HoMR1l9duSjSO1VI2aFs41ZB4UyveqfTF8SzbYSpZAjlzUw6tNb3GT1d8J0iL4CkvLS8PdmwKFrgnVJXvro4XLu0QBugSjZAcvDz8ZBb1wss79",
-                Cookie = "sb=vztBX8DzMAxkFTXd-H0Q9EI0; datr=vztBX2o97re7G40FJl0k8Qzg; c_user=100014832601900; spin=r.1002596222_b.trunk_t.1598925693_s.1_v.2_; dpr=1.25; xs=18%3AC0Nfn0v3fZ6AGQ%3A2%3A1598286483%3A17580%3A602%3A%3AAcU9DMrwuj7gG7cZb9f6yMJlLKrNT2RwVJSPBmFvig; fr=1sOz9gXDZapKtf9iE.AWXfJJW-XFimqr3z9oh8h79Elg4.BfQTu_.GS.AAA.0.0.BfToTH.AWVxbJqd"
+                Token = "EAAAAZAw4FxQIBAIhGGvb0jRpJCPMbGzftpudSZBOcm7hxVrkq8GiXf2iWZBgdpdwtR5AF0mpkS2XGLfZCRtYwWwPiBvZAApJLfKu3zO84O4OgHQccFWFDdBMaQG0zZB9ks2cnZAxvI0aXReWQLuEAZB7wiOrsbVNhocv5WeQ3zGWesuYMMuDOk4G",
+                Cookie = "sb=uuZAX8zu-ekbY50TQ0DuoH86; datr=uuZAX6_FYi0IBe4DqXYkedR-; dpr=1.25; wd=1536x722; locale=vi_VN; c_user=100016551455570; xs=29%3A9U2fJbvf8GPIjA%3A2%3A1599061415%3A17580%3A636; fr=1cABxZlerDaDKX7To.AWVndLV3wx7Bo2DS7fowySuDqEY.BfQOa6.fr.AAA.0.0.BfT72n.AWWrO_gj; spin=r.1002607989_b.trunk_t.1599061419_s.1_v.2_"
             },
-            ////muathudep58@gmail.com
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBABBK0ZCJ6fHiyuQaOHfyTjTHlWZCRw7yJDeCZBSxZBrgnfzc9zljSNfZC3N7ZCuMCGYESk81ZAMvs2CSDoQvT3R8sTdDfowUPCbJ9HLN7u6Rhf2ywtDNSolJLOMDETbEBdCdUyEZAHUiGNSsb7JC6qGSHJleNS5xLAnZAHPeVwrE4",
-            //    Cookie = "sb=xu9AXyBTsvz6rQw_mvDWxQYh; datr=xu9AX34aUBYTrUBSV5usETpO; dpr=1.25; wd=1536x722; locale=vi_VN; c_user=100014450202805; spin=r.1002596222_b.trunk_t.1598925757_s.1_v.2_; xs=36%3ANjBzeW39w7xCQg%3A2%3A1598679884%3A17580%3A636%3A%3AAcXJy92A_IqY46DexgpM8An1VzQpeaaYpZNPsILKRg; fr=1d03kp3slxKi0cVmE.AWUiP8LUWe557ZWvum7KFdErVHw.BfQO_G.AE.AAA.0.0.BfToTe.AWWOKcip"
-            //}
-
-            //100035923812881
+             //muathudep75@gmail.com
             new TokenCookie
             {
-                Token = "EAAAAZAw4FxQIBAD9MkKCsBFzO8wP29ZAZB2tZBkN3J906CRCrIxvioOcM8BWO67j2bjsdnfs5mVkwrx77gmU6eneIP0ZCF4T4LC8gVZAttfLF0OnkaaVku034vhJV3adgszvlWXUN0PxViZB88WbfliM3ltRKn7B2EeyM0JKgAfjIfqXcS4pUoHXChmZClnNuvEZD",
-                Cookie = null
+                Token = "EAAAAZAw4FxQIBAKN5XcMGMsTXylHsCZC8pGipQVdGhosfH86RoMAtSV5rsOkeRwydvcGRCbspRtsXZBmYPAGOnuetJldAYY9VZAdvlSmHdd0LFDu95WkG94bUhxZC49MwYjCr0a1wyJlt8NLMsOzF06oQfgfaackqJUK2eDGWp7ZCEjSQfmOzT",
+                Cookie = "sb=uuZAX8zu-ekbY50TQ0DuoH86; datr=uuZAX6_FYi0IBe4DqXYkedR-; dpr=1.25; wd=1536x722; locale=vi_VN; c_user=100016551455570; xs=29%3A9U2fJbvf8GPIjA%3A2%3A1599061415%3A17580%3A636; fr=1cABxZlerDaDKX7To.AWVndLV3wx7Bo2DS7fowySuDqEY.BfQOa6.fr.AAA.0.0.BfT72n.AWWrO_gj; spin=r.1002607989_b.trunk_t.1599061419_s.1_v.2_"
             },
-            //100036661600247
+              //muathudep58@gmail.com
             new TokenCookie
             {
-                Token = "EAAAAZAw4FxQIBAJyw3dUZBNjK0WMq5sn5Ak45HBGyMDQbu0kyTul1JOjc0PkDTrKiWEEP1JhPUH9XYsO7ha1cZCG8WIl6XU5mTPsxiAeKoBYPqMfbNB0ojWEgSicW8ZAC6lzamXTJ58mCfa77ruZAqbqG2Mnv2xIZAOPgKsmxqZCPW9zzZAilO5y",
-                Cookie = null
-            },
-            //100040433988861
-            new TokenCookie
-            {
-                Token = "EAAAAZAw4FxQIBAL8FEmngquUFA3LhZAYZBmXiQ92sXZCOcm0MSZAp4p3IR4qi7K7kJcoXx1dHmQtTqCASnPzvNtSX6eWEUhpLZCoatNxZAD8aajiJ4qf976t6ETsUcvhU8khuZAQr41icSKWDtIpanKtAF7nCLvA5FTOoM0nIMHeO44VEYv6aB7ZB",
-                Cookie = null
+                Token = "EAAAAZAw4FxQIBACOnwmECpbP53Cmil2Idi5sf3MpK5tDamlOmZBDC5omkcbIZB5TuD0dtNp1zZCDZCrKu8N1DZBvQIVOucStozWH1ZCin5u8Rb1JEC4JOJPTKXdGHDnRDwulBIrXKUmrrEN3UwCIidDvQ8D7Y4wrS76qZCXEhAAnJqphlycWqudf",
+                Cookie = "sb=uuZAX8zu-ekbY50TQ0DuoH86; datr=uuZAX6_FYi0IBe4DqXYkedR-; dpr=1.25; wd=1536x722; locale=vi_VN; c_user=100016551455570; xs=29%3A9U2fJbvf8GPIjA%3A2%3A1599061415%3A17580%3A636; fr=1cABxZlerDaDKX7To.AWVndLV3wx7Bo2DS7fowySuDqEY.BfQOa6.fr.AAA.0.0.BfT72n.AWWrO_gj; spin=r.1002607989_b.trunk_t.1599061419_s.1_v.2_"
             },
 
-            //100041830513494
-            new TokenCookie
-            {
-                Token = "EAAAAZAw4FxQIBABOEzSPDhWmuerG6ZC0gmjdcfSv41J2GcKZBPhUiF3JVqCQsyj8ae7MHUCRiKh9HFjzorLlD5navvKcXHxDK3KZAptB99l0lLehGbOzmQ3gWOfzYhvAARUu5EgzIGJHZCNclpNceO01KZBA6Mh8OuGVGZAkpQTq8VdgjRh1y3r",
-                Cookie = null
-            },
-            ////100044974004827
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBAPSyHYt2yOZCSAML4DdlV8scGYYZCvS0ZAYuoITxYtcJtYsQaGTShdoKLZCTEw6Q2LnwpHM1gIbmRhiaO3jg1CnLQ65oJ9Kr3EYQJyxLTzBaRJb3F8irTUjkNO1gkhUxp9FxAop8OsfhjIZBW6N4Y4RIAANlSYaPTxoIaERyHQNA5llt0rycZD",
-            //    Cookie = null
-            //},
-            //100048332077418
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBAEzNsIh6DiGtZAprfMXV1kl1bM53WvBxU3ysWZBXA6RHpWBbIBj80EnPRhXPOxam1ZAjkZAV7n9SF2Q7qjb3d8GIX4Qj8ZBgZCsBC8kkMOa18yZCuvWinKmxZBQsAGpydiYi76xTliBOFLW560fmpBQQuZCCYqPfRmSHftNT55cqklExhvKrmcjIZD",
-            //    Cookie = null
-            //},
-            //100051025834573
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBAKDm6n7xFanRZBjTPN0nWZAbtcifvDbe1y0hQIvpg16dBUZAoSTnh4iynjs2ZCVf4g4ZBLHxt36zzB0xT5Bs5CbEffZAMcQjFhyMohq4X9TNOefQB5STxqGRmSazKRexoQyIFfhaHLzFtG5uh3lQIW6SZBrweWhYL0g41sMgCMr6FxbTdsh2mwZD",
-            //    Cookie = null
-            //},
-            //100052561338020
-            new TokenCookie
-            {
-                Token = "EAAAAZAw4FxQIBABufZC4qGllrbhYMEZBkoMIOPU0OitM3LTwxqPxvAzGHyldjFNKqxycV9c1MvYvmjZAZApPl4gnoKipbvv4EPoMszBXqeYbgnq3TccEKTFaoK2yPeu0FP47CKQ7wPl7H9uXAtwSSmoZC88et3ZCjNcH53M2udazCQgZA2wIZAvQ87NLS7QMGvoYZD",
-                Cookie = null
-            },
-            //100052700708542
-            new TokenCookie
-            {
-                Token = "EAAAAZAw4FxQIBAJ5zOyG9JuJuzRBrZCNXAzsJtJLeNKqdUAFJ4XspC7C1mnnsZCuGaOsVUhhhAmb6SIE4XVtgbcgaixB7fZCgT8euB6RgnqM1oReXdTOKOBeCZAZACfZBNzvlUfA3mToLd01AN6cftrZBUecIPyZA6nF5oVM5hY9OdigxbC5E5xuZBiklxt82aUqEZD",
-                Cookie = null
-            },
-            //100053098960149
-            new TokenCookie
-            {
-                Token = "EAAAAZAw4FxQIBALDvgI5lfgN5P1tQvpSLL10AgZAY2CFmicLBYN20SmjDVaec6WtZBp9lazVcP6dEe5aF1D9VznPIZBXo9kztZCuwShiiAoZAGYiWWByTaV6PkUDUqb8xz0Px0ZBZBwt10xyCJuuRzZAy7QH4Vf7BJfPSOCdYUWeuvwvqichfH2TMXUcM2QawQIEZD",
-                Cookie = null
-            },
-            ////100054001928721
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBAHhJStkvywIM2KhUZC1mXlg4vgIGeHO2zmWsMwMdNvB1OtiaE6glSUlsKcMi9OKZAPrzjG2ZBcgETZCJsTOMFNaaUF2eA5r5YwJ8ADryv4hSiSrnLOBWJCZCGXhNc6jxJ1kPyw1B5tnq8drI0BaIuLZBOWxyRAArnLjHwHZAbF9wsEUvbmGgFMZD",
-            //    Cookie = null
-            //},
-            //100029675925921
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBABUUlzsqnuEaHaG6YTDiF2qCPgqIVY0p2yQZC1Q49VHoXrakzDZAxZCxBOUhJGHk5lTuALdvL6WdaBdJ9aoc3qUBhOsZCNsyqwuNZAclDlnpnMGtUu4GkwiuGuTVXrvCZCOSWemKVZAjGYFsOZByx0j3e89s814QFstFFF4Dg53jWOm6ZCa9F4OMZD",
-            //    Cookie = null
-            //},
-            //100029915462948
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBAIvSoFQ8EBg67BM8Yj1GGZCD0RSd19Hy4WouwZARz0D4FJvwKPYtaxaqGtBjWscVBEoAWTjbeVWlfZBRisJ3OG3dZAMPMhodeFcg5DSNxMiaQraJW8zOQYHZA9ZBM1nxMmIOUqRNGqMn8Ilt6ZBaqs3R7MYslAJ4EfY2hG1t4uWpDlUUXgZCBqwZD",
-            //    Cookie = null
-            //},
-            //100035669018060
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBABtgkcstV1HMH5QqYqrPzWfi4FipZCPsqSDUKoMFD87ocKXmBJiHRA56EE6GdDcwwRwO9paZAOCqxZA8xQInI0GBDUDCcXu1hAKrxZCy4F05dxZCyb3BKiYnJT0xwf9eiJZAEEd0XwdIZCBhP9gArlVZAQVsr5DIDQCrCgiWdZBSs1GZCLMx1qKZAwZD",
-            //    Cookie = null
-            //},
-
-            //100040636296591
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBAIdh7QDWpsgI47pBJgEVWbsnTtC3yvZBZCHRCzB0ahT3CLtEJ5F74BLvaNCwlMXqet916oy83VegJ495NG6gPVQfgtkWrl4eqZAXc8C2vQ1d43UlmwBcx69IVgsZAvwZCwoDyb7E2kkkL3Q3mKtu3PD7f9I9Uq5ITPtZATlDzsVzaxqGZCtgP4ZD",
-            //    Cookie = null
-            //},
-
-            ////100049314679812
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBACpOkHZACFIROjyYrS5UivIhqzP5oGa39IuN3vFB7cep1zNQxtfr1iqsrBetef9AZBxieBRjUZBCfDxLKVoMagz5KMvOn4B1N9VTwuwxWdSb99uZC3VbrHFI2XpDNGijTvGFW5J77zlHIocrrE4DDzlGi1rJ3ZCtUI5NJt1Yz",
-            //    Cookie = null
-            //},
-
-
-            //100050790709539
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBACUhaEiYAurZBoOyEayfZAoK2O7cF9uZBBkgQXp11qNlmG3w5dc7pIS52rQ8ulEWZB7etJP6oJPjO2PdVhnT5b5eglrHQnHOwkZBiXJ6Cbaz8MeI8JeGOM6ALXfGjZBTFEpKT4ZCcpz1LPiemWtmrfVsEjRXLjdtySOY8ybSqgnkBkLOkaZCSqcZD",
-            //    Cookie = null
-            //},
-
-
-            ////100051051522091
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBAArZA2MaZCLHoDVq4CULDaHQD4jccuJvZCjZAG0MRkaNbF7kO1B004RdO8T5iCjM7l0y9V6rDU0RZBIrPGgH40zQSIdyZBCSEgj5ZBMoD1DdLz4SruLzArld5W2Lb2Q3ugYrshM56C1c8fcJLVqhIjMcWOySB3wLt1DQgcf1DXiLgksCxI6ZBywZD",
-            //    Cookie = null
-            //},
-
-
-            ////100052443877358
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBAOilpVQL0K2EgWJS6XPEnZCQZASKiaihg2jEvuXjcpAP3ZBXWnZBeq0H8TohmIjji1sjZB2uL3OCN5EY3k236qwiVZCKSaELDNYnVfT2WDWcBj2jijaHZAfTLV6Xe5ZB2tAzGZB9eiAOrUzid8KZCToxKoZBqO0pvMREql2dZCenbgP5",
-            //    Cookie = null
-            //},
-            //100055243302760
-            //new TokenCookie
-            //{
-            //    Token = "EAAAAZAw4FxQIBADl1TpcJsikeQJXYFs207slIu0fPpsbBB0hhnmlbD2x99aOGcbTfMoZApOnv56krp2HoTD9CVLGpvjZC0OrZA2VKM4hJzZAzdel9AkZBrVAJV23uWwygsRXR26gZCeYsPdLA0CwTXfT7GaKMqWhxZBqUD376ZBmrCu6n5OkZA2FWz",
-            //    Cookie = null
-            //},
         };
     }
 }
